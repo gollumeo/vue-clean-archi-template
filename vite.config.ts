76532a5402052f8bd6294a36e9b41209825da25b
Golluméo <p.mauriello54@gmail.com>
2025-06-16 16:51:11 +0200

    feat(project-setup): add Vite configuration with alias support
    Introduced a `vite.config.ts` file using the Vite framework. Configured Vue support and added path aliases for key architecture layers (e.g., `@application`, `@domain`). This enhances code organization and simplifies module imports.

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@application': path.resolve(__dirname, './src/Application'),
+      '@domain': path.resolve(__dirname, './src/Domain'),
+      '@framework': path.resolve(__dirname, './src/Framework'),
+      '@infrastructure': path.resolve(__dirname, './src/Infrastructure'),
+      '@persistence': path.resolve(__dirname, './src/Persistence'),
+      '@presentation': path.resolve(__dirname, './src/Presentation'),
+      '@cli': path.resolve(__dirname, './src/Presentation/CLI'),
+      '@tests': path.resolve(__dirname, './src/Presentation/Tests'),
+    },
+  },
+});
