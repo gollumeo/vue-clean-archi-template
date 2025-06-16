51a64b95c0816d77580bb9d52637baaa79d9fa34
Golluméo <p.mauriello54@gmail.com>
2025-06-16 17:33:46 +0200

    feat(testing): add unit test for App component
    Added a unit test for the `App` component to verify rendering of the heading text. Utilized `@vue/test-utils` and the Vitest framework for the implementation. This marks the addition of the first component test, enhancing test coverage.

---
+++ src/Tests/Presentation/AppDummy.test.ts
@@ -0,0 +1,9 @@
+import App from '@framework/Vue/App.vue';
+import { mount, VueWrapper } from '@vue/test-utils';
+
+describe('App', () => {
+  it('renders the heading', () => {
+    const wrapper: VueWrapper = mount(App);
+    expect(wrapper.text()).toContain('coucou');
+  });
+});
