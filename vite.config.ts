6529134f7cbbc974a2763fc8bfa19f57690fe629
Golluméo <p.mauriello54@gmail.com>
2025-06-17 12:48:16 +0200

    refactor(config): adjust formatting in `vite.config.ts` for readability
    Reorganized the formatting of the `vite.config.ts` file to improve readability. This includes adjusting indentation and rearranging imports to maintain consistency and clarity in the configuration file. No functional changes were introduced.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { defineConfig, UserConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@application': path.resolve(__dirname, './src/Application'),
-      '@domain': path.resolve(__dirname, './src/Domain'),
-      '@framework': path.resolve(__dirname, './src/Framework'),
-      '@infrastructure': path.resolve(__dirname, './src/Infrastructure'),
-      '@persistence': path.resolve(__dirname, './src/Persistence'),
-      '@presentation': path.resolve(__dirname, './src/Presentation'),
-      '@cli': path.resolve(__dirname, './src/Presentation/CLI'),
-      '@tests': path.resolve(__dirname, './src/Presentation/Tests'),
+    plugins: [
+        vue(),
+        tailwindcss()
+    ],
+    resolve: {
+        alias: {
+            '@application': path.resolve(__dirname, './src/Application'),
+            '@domain': path.resolve(__dirname, './src/Domain'),
+            '@framework': path.resolve(__dirname, './src/Framework'),
+            '@infrastructure': path.resolve(__dirname, './src/Infrastructure'),
+            '@persistence': path.resolve(__dirname, './src/Persistence'),
+            '@presentation': path.resolve(__dirname, './src/Presentation'),
+            '@cli': path.resolve(__dirname, './src/Presentation/CLI'),
+            '@tests': path.resolve(__dirname, './src/Presentation/Tests'),
+        },
     },
-  },
 } as UserConfig);
