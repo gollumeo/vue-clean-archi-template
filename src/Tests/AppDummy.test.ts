578f7db63fed4500236def731d7cd1e9dcca84f9
Golluméo <p.mauriello54@gmail.com>
2025-06-17 12:59:52 +0200

    style(tests): standardize indentation in `AppDummy.test.ts`
    Adjusted indentation in the `AppDummy.test.ts` file to ensure consistency with the project's coding style guidelines. This change improves readability without affecting the test logic.

--- src/Tests/AppDummy.test.ts
+++ src/Tests/AppDummy.test.ts
@@ -1,9 +1,12 @@
 import App from '@framework/Vue/App.vue';
 import { mount, VueWrapper } from '@vue/test-utils';
 
-describe('App', () => {
-  it('renders the heading', () => {
-    const wrapper: VueWrapper = mount(App);
-    expect(wrapper.text()).toContain('coucou');
-  });
+describe('App', () => 
+{
+    it('renders the heading', () => 
+{
+        const wrapper: VueWrapper = mount(App);
+        expect(wrapper.text()).toContain('coucou');
+    });
 });
+ 
