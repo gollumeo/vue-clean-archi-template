73f0d5ab52e9fd819cfcebdfab9321846b26c5b9
Golluméo <p.mauriello54@gmail.com>
2025-06-19 01:19:13 +0200

    style(tests): improve indentation and add new test case in `Acherus.test.ts`
    Refactored the indentation style and added a new test case for `Acherus` ensuring it scaffolds a use-case file in `Application` and its test file in `Tests/Application`. These changes maintain consistency with existing coding standards and enhance test coverage.

--- src/Tests/Presentation/CLI/Acherus.test.ts
+++ src/Tests/Presentation/CLI/Acherus.test.ts
@@ -2,30 +2,42 @@ import { Acherus } from '@cli/Acherus';
 import * as fs from 'node:fs';
 import path from 'path';
 
-describe('Acherus CLI', () => {
+describe('Acherus CLI', () => 
+{
     const outputDir = path.resolve(__dirname, '../../../__sandbox__/Acherus');
+    const acherus = new Acherus(outputDir);
 
-    beforeEach(() => {
+    beforeEach(() => 
+    {
         fs.mkdirSync(outputDir, { recursive: true });
     });
 
-    afterEach(() => {
+    afterEach(() => 
+    {
         fs.rmSync(outputDir, { recursive: true, force: true });
     });
 
-    it('should scaffold a basic use-case in Application and its test file', () => {
-        const acherus = new Acherus(outputDir);
-        const action: string = 'use-case';
+    it('should scaffold a basic use-case in Application and its test file', () => 
+    {
         const useCaseName: string = 'IntroduceTestUseCase';
 
-        acherus.make(action, useCaseName);
+        acherus.make('', useCaseName);
 
         expect(
             fs.existsSync(path.resolve(path.join(outputDir, 'Application', `${ useCaseName }.ts`)))
         ).toBeTruthy();
 
         expect(
-            fs.existsSync(path.resolve(path.join(outputDir, 'Application', `${ useCaseName }.test.ts`)))
+            fs.existsSync(path.resolve(path.join(outputDir, 'Tests', 'Application', `${ useCaseName }.test.ts`)))
         ).toBeTruthy();
     });
+
+    it('should create the use case file in Application and its test in Tests/Application', () => 
+    {
+        const useCaseName: string = 'ScheduleInterview';
+        acherus.make('', useCaseName);
+
+        expect(fs.existsSync(path.join(outputDir, 'Application', `${ useCaseName }.ts`))).toBeTruthy();
+        expect(fs.existsSync(path.join(outputDir, 'Tests', 'Application', `${ useCaseName }.test.ts`))).toBeTruthy();
+    })
 });
