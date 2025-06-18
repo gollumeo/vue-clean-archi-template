82b18300f0e2bfbd18a800c7f5d1967bf9d3d1a0
Golluméo <p.mauriello54@gmail.com>
2025-06-19 01:19:18 +0200

    feat(cli): update directory paths for test scaffolding in `Acherus.ts`
    Modified the `make` method in `Acherus.ts` to separate test files into a dedicated `Tests/Application` directory. This ensures logical organization of generated test files and improves project structure for better maintainability.

--- src/Presentation/CLI/Acherus.ts
+++ src/Presentation/CLI/Acherus.ts
@@ -10,10 +10,12 @@ export class Acherus
     make(_: string, name: string): void
     {
         const applicationDir: string = path.join(this.outputDir, 'Application');
+        const testsDir: string = path.join(this.outputDir, 'Tests/Application');
         fs.mkdirSync(applicationDir, { recursive: true });
+        fs.mkdirSync(testsDir, { recursive: true });
 
         const useCasePath: string = path.join(applicationDir, `${ name }.ts`);
-        const testPath: string = path.join(applicationDir, `${ name }.test.ts`);
+        const testPath: string = path.join(testsDir, `${ name }.test.ts`);
 
         fs.writeFileSync(useCasePath, `export class ${ name } {}`);
         fs.writeFileSync(testPath, `describe('${ name }', () => {});`);
