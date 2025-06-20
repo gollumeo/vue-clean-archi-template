3c481d24229f83eb4fdd2ef86feb2f8b5bb241f8
Golluméo <p.mauriello54@gmail.com>
2025-06-20 09:44:21 +0200

    feat(cli): add scaffolding logic to `Acherus`
    Introduced a `make` method in `Acherus` for scaffolding use case files and corresponding test files. The method creates directories and files under `Application` and `Tests` paths, ensuring a streamlined process for file generation.

--- src/Presentation/CLI/Acherus.ts
+++ src/Presentation/CLI/Acherus.ts
@@ -1,4 +1,18 @@
+import * as fs from "node:fs";
+import path from "path";
+
 export class Acherus
 {
+    constructor(private outputDir: string)
+    {
+    }
 
+    make(name: string)
+    {
+        const applicationDir: string = path.join(this.outputDir, 'Application');
+        fs.mkdirSync(applicationDir, { recursive: true });
+
+        fs.writeFileSync(path.join(applicationDir, name + '.ts'), '');
+        fs.writeFileSync(path.join(this.outputDir, name + '.test.ts'), '');
+    }
 }
